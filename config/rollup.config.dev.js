c95ed68dbea7d042dcb0dfcb364370a66b1dd116
jrtnq514 <burggamer.jt@gmail.com>
2020-10-02 16:31:21 -0500

    feat(rollup.config.dev.js): added a dev config for rollup that includes a dev server

---
+++ config/rollup.config.dev.js
@@ -0,0 +1,31 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import serve from "rollup-plugin-serve";
+
+export default {
+  input: 'src/rollup.js',
+  // Messing around with different output types. May need differnet input depending on format
+  // Also need to do research on which formats we should build
+  output: [
+    {
+      file: 'dist/rollup.umd.js',
+      format: 'umd',
+      name: 'rollupUMD'
+    },
+    {
+      file: 'dist/rollup.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/rollup.es.js',
+      format: 'es'
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({ babelHelpers: 'inline' }),
+    commonjs(),
+    serve('dev-server')
+  ]
+};
